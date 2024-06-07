f0c4fb299134d2032c85179cfba9eb4b92e3c513
SilenceEnjoyer <liadchk@gmail.com>
2024-06-07 14:32:25 -0400

    finished

--- 05-BouncingBall/BouncingBall.py
+++ 05-BouncingBall/BouncingBall.py
@@ -6,29 +6,85 @@ import random
 # You will implement this module ENTIRELY ON YOUR OWN!
 
 # TODO: Create a Ball class.
-# TODO: Possible member variables: screen, color, x, y, radius, speed_x, speed_y
-# TODO: Methods: __init__, draw, move
+class Ball:
+    def __init__(self, screen):
+        self.screen = screen
+        self.x = random.randint(0, screen.get_width())
+        self.y = random.randint(0, screen.get_height())
+        self.rad = random.randint(10, 35)
+        self.speed_x = random.randint(-3, 3)
+        self.speed_y = random.randint(-3, 3)
+        self.r = random.randint(0, 255)
+        self.g = random.randint(0, 255)
+        self.b = random.randint(0, 255)
+
+
+    def draw(self):
+        pygame.draw.circle(self.screen, (self.r, self.g, self.b), (self.x, self.y), self.rad, 2)
+
+
+    def OffScreen(self):
+        if self.y > self.screen.get_height() or self.y < 0:
+            return 1
+        elif self.x > self.screen.get_width() or self.x < 0:
+            return 2
+        else:
+            return 0
+
+
+    def move(self):
+        self.y += self.speed_y
+        self.x += self.speed_x
+        if self.OffScreen() == 1:
+            self.speed_y = -self.speed_y
+        elif self.OffScreen() == 2:
+            self.speed_x = -self.speed_x
+        else:
+            self.speed_x = self.speed_x
+            self.speed_y = self.speed_y
+
+
+
+
+
+
+# : Possible member variables: screen, color, x, y, radius, speed_x, speed_y
+# : Methods: __init__, draw, move
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((300, 300))
+    screen = pygame.display.set_mode((1000, 800))
     pygame.display.set_caption('Bouncing Ball')
     screen.fill(pygame.Color('gray'))
     clock = pygame.time.Clock()
+    ball = Ball(screen)
 
-    # TODO: Create an instance of the Ball class called ball1
+
+
+
+
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+
         clock.tick(60)
         screen.fill(pygame.Color('gray'))
 
-        # TODO: Move the ball
-        # TODO: Draw the ball
+        ballList = []
+        for i in range(100):
+            ballList.append(Ball(screen))
+
+        for ball in ballList:
+            ball.draw()
+            ball.move()
+
+
+
 
         pygame.display.update()
 
